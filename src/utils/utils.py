45a7edacb4c492346b51b83c95d361771e084f58
Yunus Emre Kara <yunuskr40@gmail.com>
2025-04-24 20:35:04 +0200

    refactoring

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,16 +1,16 @@
-from src.data_preprocessing import load_and_preprocess_data, load_and_preprocess_multiclass_data, \
-    load_and_preprocess_webattack_data
+from src.data_preprocessing import load_and_preprocess_dataset1, load_and_preprocess_dataset2, \
+    load_and_preprocess_dataset3
 
 
 def get_data():
     train_path = 'data/input/dataset1_US_LAN.csv'
-    return load_and_preprocess_data(train_path)
+    return load_and_preprocess_dataset1(train_path)
 
 def get_data_multiclass():
     train_path = 'data/input/dataset2_UNSW_NB15.csv'
-    return load_and_preprocess_multiclass_data(train_path)
+    return load_and_preprocess_dataset2(train_path)
 
-    #train_path = 'data/input/dataset3_WebAttacks.csv'
-    #return load_and_preprocess_multiclass_data(train_path)
+    # train_path = 'data/input/dataset3_WebAttacks.csv'
+    # return load_and_preprocess_dataset3(train_path)
 
 
