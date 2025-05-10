46fdbcaaee746996f0ad38ef7ab5d1dbc14d0537
Yunus Emre Kara <yunuskr40@gmail.com>
2025-05-10 19:11:07 +0200

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from src.multiclass.evaluate_models_multiclass import evaluate_models_multiclass
-from src.multiclass.train_models_multiclass import train_and_save_model_multiclass
+from src.multiclass_classification.evaluate_models_multiclass import evaluate_models_multiclass
+from src.multiclass_classification.train_models_multiclass import train_and_save_model_multiclass
 from src.utils.utils import get_data, get_data_multiclass
-from src.binary.train_models_binary import train_and_save_models_binary
-from src.binary.evaluate_models_binary import evaluate_models_binary
+from src.binary_classifikation.train_models_binary import train_and_save_models_binary
+from src.binary_classifikation.evaluate_models_binary import evaluate_models_binary
 
 
 def main():
